f7dc6815c90470425184c384534a14abca358065
Roan Morgan <roanlm007@gmail.com>
2023-02-22 11:49:52 -0800

    brain melt

--- VSCODE-Labs/Lab3b/LinkedListOperators.cpp
+++ VSCODE-Labs/Lab3b/LinkedListOperators.cpp
@@ -3,28 +3,120 @@
 //2.16.23
 #include <iostream>
 #include <cstdlib>
+#include <string>
 using namespace std;
 
-struct Node{
+struct Node {
     Node* next;
     string contents;
 
 };
 
-int main(void){
+void print(Node* arg);
+void printR(Node* arg);
+int count(Node* arg);
+
+int main(void) {
 
     Node* a = nullptr; //LL
     Node* cur = nullptr; //iterator
 
+    bool isFirst = true;
     char userin = 'y';
+    string temp = "N/A";
 
-    do{
+    cout << endl;
 
+    do {
 
-        cout << "\n would you like to add another element (y/n):";
+        if (isFirst) {
+            cur = new Node();
+            cout << "Input a word: ";
+            cin >> temp;
+            cur->contents = temp;
+
+            a = cur;
+
+            isFirst = false;
+        }
+        else {
+            cur->next = new Node();
+            cur = cur->next;
+            cout << "Input a word: ";
+            cin >> temp;
+            cur->contents = temp;
+        }
+
+        cout << "\twould you like to add another element(y / n) :";
         cin >> userin;
-        
-    }while(userin == 'y');
+
+        cout << endl;
+
+    } while (userin == 'y');
+
+    cur->next = nullptr;
+
+    cur = a;
+
+    cout << "Forwards: ";
+    print(a);
+    cout << endl;
+
+    cout << "Elements #: " << count(a) << endl;
+
+    cout << "Backwards: ";
+    printR(a);
+    cout << endl;
+
+    delete a;
 
     return(0);
+}
+
+void print(Node* arg)
+{
+    Node* cur = arg;
+
+    do {
+        cout << cur->contents << " ";
+        cur = cur->next;
+
+    } while (cur != nullptr);
+}
+
+void printR(Node* arg)
+{
+    Node* cur = arg;
+    Node* prev = nullptr;
+    Node* next = nullptr;
+
+    do {
+        next = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = next;
+    } while (cur != nullptr);
+
+    cur = prev;
+
+    do {
+        cout << cur->contents << " ";
+        cur = cur->next;
+
+    } while (cur != nullptr);
+     
+}
+
+int count(Node* arg)
+{
+    Node* cur = arg;
+    int count = 0;
+
+    do {
+        cur = cur->next;
+        count++;
+    } while (cur != nullptr);
+
+    return count;
+
 }
